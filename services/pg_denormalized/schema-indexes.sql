61fb24cf0ca430785c7640800b1dc20043f0137a
Shreya Subramanian <shsubramanian@g.hmc.edu>
2026-04-29 16:37:15 -0700

    times fixed missing index added

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -1,4 +1,3 @@
 CREATE INDEX ON tweets_jsonb USING GIN((data->'entities'->'hashtags') jsonb_path_ops);
 CREATE INDEX ON tweets_jsonb USING GIN((data->'extended_tweet'->'entities'->'hashtags') jsonb_path_ops);
-CREATE INDEX ON tweets_jsonb USING BTREE((data->>'lang'));
 CREATE INDEX ON tweets_jsonb USING GIN(to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text')));
--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -2,3 +2,4 @@ CREATE INDEX ON tweet_tags(tag);
 CREATE INDEX ON tweet_tags(id_tweets);
 CREATE INDEX ON tweets(lang);
 CREATE INDEX ON tweets USING GIN(to_tsvector('english', text));
+CREATE INDEX ON tweets(id_tweets);
